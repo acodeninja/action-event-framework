0c81d1ce575e4e40bf34bd7ce3998a3d09eea1aa
Lawrence <lawrence@acode.ninja>
2021-07-27 11:09:03 +0100

    Add a pascal case parser

---
+++ src/Helpers/String.test.ts
@@ -0,0 +1,15 @@
+import {describe, expect, test} from "@jest/globals";
+
+import {parsePascal} from "./String";
+
+describe('parsing a pascal string', () => {
+  const parsed = parsePascal('Test');
+
+  test('produces an array', () => {
+    expect(parsed).toBeInstanceOf(Array);
+  });
+
+  test('has lower cased the elements of the array', () => {
+    expect(parsed).toEqual(['test']);
+  });
+});
---
+++ src/Helpers/String.ts
@@ -0,0 +1,9 @@
+export const parsePascal = (input: string): Array<string> => {
+  const output = input.replace(/([A-Z])/g, ' $1')
+    .toLowerCase()
+    .split(' ')
+
+  output.shift();
+
+  return output;
+}
