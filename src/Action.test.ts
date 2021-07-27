980dfc04f2a822aa6ccfdbaa1c893519584ddb16
Lawrence <lawrence@acode.ninja>
2021-07-27 11:37:24 +0100

    Add static create method to the action class

--- src/Action.test.ts
+++ src/Action.test.ts
@@ -6,9 +6,14 @@ class TestAction extends Action {
 }
 
 describe('a newly instantiated Action', () => {
-  const event = new TestAction();
+  const payload = {test: 'test'};
+  const event = TestAction.create(payload);
 
   test('has an appropriate event name', () => {
     expect(event).toHaveProperty('action', 'test:action');
+  });
+
+  test('has the input payload', () => {
+    expect(event).toHaveProperty('payload', payload);
   });
 });
--- src/Action.ts
+++ src/Action.ts
@@ -2,10 +2,21 @@ import {parsePascal} from "./Helpers/String";
 
 export interface ActionInterface {
   readonly action: string;
+  payload?: Record<string, unknown>;
 }
 
 export class Action implements ActionInterface {
+  payload?: Record<string, unknown>;
+
   get action() {
     return parsePascal(this.constructor.name).join(':');
+  }
+
+  static create(payload: Record<string, unknown>) {
+    const action = <ActionInterface>new this();
+
+    action.payload = payload;
+
+    return action;
   }
 }
