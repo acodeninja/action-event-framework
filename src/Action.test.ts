fcc408e019de346ace5a9dace367d76d08ca1add
Lawrence <lawrence@acode.ninja>
2021-07-27 11:55:22 +0100

    Add execute and implementation to actions

--- src/Action.test.ts
+++ src/Action.test.ts
@@ -1,19 +1,36 @@
-import {describe, expect, test} from "@jest/globals";
+import {beforeAll, describe, expect, jest, test} from "@jest/globals";
 import {Action} from "./Action";
 
 class TestAction extends Action {
+  async implementation(): Promise<void> {
 
+  }
 }
 
-describe('a newly instantiated Action', () => {
+describe('creating an Action instance', () => {
   const payload = {test: 'test'};
-  const event = TestAction.create(payload);
+  const action = TestAction.create(payload);
 
-  test('has an appropriate event name', () => {
-    expect(event).toHaveProperty('action', 'test:action');
+  test('has an appropriate action name', () => {
+    expect(action).toHaveProperty('action', 'test:action');
   });
 
   test('has the input payload', () => {
-    expect(event).toHaveProperty('payload', payload);
+    expect(action).toHaveProperty('payload', payload);
+  });
+});
+
+describe('executing an Action', () => {
+  const payload = {test: 'test'};
+  const action = TestAction.create(payload);
+
+  action.implementation = jest.fn(action.implementation);
+
+  beforeAll(async () => {
+    await action.execute();
+  });
+
+  test('the action implementation has been called', () => {
+    expect(action.implementation).toHaveBeenCalled();
   });
 });
--- src/Action.ts
+++ src/Action.ts
@@ -3,13 +3,20 @@ import {parsePascal} from "./Helpers/String";
 export interface ActionInterface {
   readonly action: string;
   payload?: Record<string, unknown>;
+  execute(): Promise<void>;
+  implementation?(): Promise<void>;
 }
 
 export class Action implements ActionInterface {
   payload?: Record<string, unknown>;
+  implementation?(): Promise<void>;
 
   get action() {
     return parsePascal(this.constructor.name).join(':');
+  }
+
+  async execute(): Promise<void> {
+    if (this.implementation) await this.implementation();
   }
 
   static create(payload: Record<string, unknown>) {
